7ffbbc26b04e7d76b0cd4c2d2934c709857b190d
KKKode <89744365+KKKode@users.noreply.github.com>
2021-11-25 22:54:38 +0530

    Update substring.js

--- Substring/substring.js
+++ Substring/substring.js
@@ -3,25 +3,18 @@ function differentCount(s,n)
     return (s.charAt(0) == s.charAt(s.length - 1));
 }
   
-function countDifferentEnds(s)
-{
+function countDifferentEnds(s) {
+    
     var result = 0;
     var n = s.length;
   
-  
-    for (var i = 0; i < n; i++)
-  
-      
-       for (var len = 1; len <= n-i; len++)
-  
-          
+    for (var i = 0; i < n; i++)   
+       for (var len = 1; len <= n-i; len++)            
           if (! differentCount(s.substring(i, i + len)))
             result++;
   
     return result;
-}
-  
-
+} 
  
     var s = "10201";
     var n = s.length;
