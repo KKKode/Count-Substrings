a822e7e3fb22292ade9d545f428f5b021a0483e0
kishan kumar swain <kkskumarswain@gmail.com>
2021-11-25 22:51:19 +0530

    initial commit

---
+++ Substring/substring.js
@@ -0,0 +1,29 @@
+function differentCount(s,n)
+{
+    return (s.charAt(0) == s.charAt(s.length - 1));
+}
+  
+function countDifferentEnds(s)
+{
+    var result = 0;
+    var n = s.length;
+  
+  
+    for (var i = 0; i < n; i++)
+  
+      
+       for (var len = 1; len <= n-i; len++)
+  
+          
+          if (! differentCount(s.substring(i, i + len)))
+            result++;
+  
+    return result;
+}
+  
+
+ 
+    var s = "10201";
+    var n = s.length;
+    document.write(countDifferentEnds(s,n));
+ 
